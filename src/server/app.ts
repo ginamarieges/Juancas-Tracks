c5972a08842dedeea8708717edf57a8cd0476603
Gina Marieges <gina.marieges@gmail.com>
2023-07-25 23:06:43 +0200

    Add morgan middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 export const app = express();
 
@@ -10,4 +11,7 @@ const options: cors.CorsOptions = {
 };
 
 app.use(cors(options));
+app.use(express.json());
+
+app.use(morgan("dev"));
 app.disable("x-powered-by");
