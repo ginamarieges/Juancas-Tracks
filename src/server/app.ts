c40bd83cf4ad7a8c5ec812aedea6428b7ab237dc
Gina Marieges <gina.marieges@gmail.com>
2023-07-28 17:25:43 +0200

    Add pingController in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { paths } from "./paths/paths.js";
+import { pingController } from "./controllers/pingController/pingController.js";
 
 export const app = express();
 
@@ -15,3 +17,5 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 app.disable("x-powered-by");
+
+app.get(paths.root, pingController);
