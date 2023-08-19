dd5efdfe656ce6c857590e7d425637ffee1b5e04
Gina Marieges <gina.marieges@gmail.com>
2023-08-19 17:22:10 +0200

    Add login in userRouter

--- src/server/routers/users/userRouter.ts
+++ src/server/routers/users/userRouter.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { paths } from "../../paths/paths.js";
-import { registerUser } from "../../controllers/userControllers/userControllers.js";
+import {
+  loginUser,
+  registerUser,
+} from "../../controllers/userControllers/userControllers.js";
 
 const userRouter = Router();
 
 userRouter.post(paths.register, registerUser);
+
+userRouter.get(paths.login, loginUser);
 
 export default userRouter;
