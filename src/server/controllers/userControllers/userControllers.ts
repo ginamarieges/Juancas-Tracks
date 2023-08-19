42dc402f3a7d6ab8e0625528ad66259ffdd19c72
Gina Marieges <gina.marieges@gmail.com>
2023-08-19 17:17:06 +0200

    Add loginUser controller

--- src/server/controllers/userControllers/userControllers.ts
+++ src/server/controllers/userControllers/userControllers.ts
@@ -1,4 +1,5 @@
 import { type NextFunction, type Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { type CustomRequest } from "./types.js";
 import { User } from "../../../database/models/User.js";
@@ -24,6 +25,35 @@ export const registerUser = async (
     }
 
     res.status(201).json({ newUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { password, username } = req.body;
+
+  try {
+    const user = await User.findOne({ username }).exec();
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      const customError = errorMessages.wrongCredentials;
+      throw customError;
+    }
+
+    const tokenPayload: JwtPayload = {
+      sub: user._id.toString(),
+      name: username,
+    };
+
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET!, {
+      expiresIn: "100d",
+    });
+
+    res.status(200).json({ token });
   } catch (error) {
     next(error);
   }
